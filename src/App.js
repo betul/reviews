3919f56a88929a1ec850be41d2836f7c91541881
betul <bsenger@aof.anadolu.edu.tr>
2023-04-05 00:43:33 +0300

    feature: reviews app completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import CardList from "./CardList";
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title"><h2>our reviews</h2><div className="underline"></div></div>
+        <CardList />
+      </section>
+    </main>
+
+  );
+}
+
+export default App;
---
+++ src/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Review from './Card';
+
+function ReviewList() {
+  return (
+    <div className="review-list">
+      <Review />
+    </div>
+  );
+}
+
+export default ReviewList;
